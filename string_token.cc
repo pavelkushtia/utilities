05d114469de0755c249dc90972b6d0fba684a0c4
Sanzad Siddique <pavelkushtia@gmail.com>
2022-07-15 14:38:37 -0700

    Create string_token.cc

---
+++ string_token.cc
@@ -0,0 +1,40 @@
+/******************************************************************************
+Helper function for string tokenization using a set of delimeters.
+*******************************************************************************/
+
+#include <string.h>
+#include <vector>
+#include <string>
+
+// Receives a string and a list of delimeter and returns vector of delimited 
+// string. 
+std::vector<std::string> GetTokens(std::string str, std::string delimiters) {
+    std::vector<std::string> tokens;
+    char *token = strtok(str.data(), delimiters.data());
+ 
+    while (token != NULL)
+    {
+        tokens.push_back(token);
+        token = strtok(NULL, "-");
+    }
+ 
+    return tokens;
+}
+
+
+/******************************************************************************
+Exmple usage: 
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    auto result = GetTokens("Hello World! - Welcome", " -");
+    
+    for ( auto s : result ) {
+        std::cout << s << std::endl;
+    } 
+    
+    return 0;
+}
